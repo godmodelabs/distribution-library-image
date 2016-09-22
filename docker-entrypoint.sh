043a6c0eb0fbb96b8ba433d502d30c54c70a5384
Daniel <daniel.possmann@boerse-go.de>
2016-09-22 20:50:26 +0200

    added update-ca-certificates call

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# check if we should run update-ca-certificates
+mount | grep -qE '/usr/local/share/ca-certificates/.+' && update-ca-certificates --fresh
+
 case "$1" in
     *.yaml|*.yml) set -- registry serve "$@" ;;
     serve|garbage-collect|help|-*) set -- registry "$@" ;;
